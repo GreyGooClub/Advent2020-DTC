2b5a864f8c4d8f4dd45d9f4966a7f71f2795bf1e
Devin Crichton <devin.crichton@gmail.com>
2020-12-05 14:19:04 +0100

    Simplifying and neatening Day 5
    - Figured out how to do the max of a vector properly.
    - Also realised the row*8 + col means you can just interpret
      the entire string as binary and get the CID.

--- 05/aoc05.cpp
+++ 05/aoc05.cpp
@@ -1,40 +1,31 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
-#include <math.h>
 #include <string>
 #include <vector>
 
 int main() {
 
-  int max = 0;
   std::vector<int> cids;
 
+  // Read in and part 1
   for (std::string line; std::getline(std::cin, line);){
-    int cid;
-    int row = 0;
-    for (int i = 0; i < 7; i++) {
-      if (line[i] == 'B'){
-        row += std::pow(2, 6-i);
+    int cid = 0;
+    int N = line.size();
+    for (int i=0; i < N; i++) {
+      if ((line[i] == 'B') || (line[i] == 'R')){
+        cid += std::pow(2, N-1-i);
       }
     }
-    int col = 0;
-    for (int i = 7; i < 10; i++) {
-      if (line[i] == 'R') {
-        col += std::pow(2, 9-i);
-      }
-    }
-    cid = row*8 + col;
     cids.push_back(cid);
-    if (cid > max){
-        max = cid;
-     }
-    // std::cout << line << ' ' << row << ' ' << col << ' ' << cid << std::endl;
   }
-  std::cout << "Highest ticket number (Part 1): " << max << std::endl;
-  std::sort(cids.begin(), cids.end());
 
-  for (std::size_t i=0; i<cids.size()-1; i++){
+  int max = *std::max_element(cids.begin(), cids.end());
+  std::cout << "Highest ticket number (Part 1): " << max << std::endl;
+
+  // Part 2
+  std::sort(cids.begin(), cids.end());
+  for (std::size_t i=0; i < cids.size()-1; i++){
     int check = cids[i + 1] - cids[i];
     if (check> 1) {
       std::cout << "My seat (Part 2): " << cids[i]+1 << std::endl;
