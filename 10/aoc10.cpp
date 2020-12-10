cb01366e890888db833fbbff968fcffa90368f65
Devin Crichton <devin.crichton@gmail.com>
2020-12-10 14:02:07 +0100

    Neatening and generalising using Tribonacci sequence.

--- 10/aoc10.cpp
+++ 10/aoc10.cpp
@@ -4,6 +4,17 @@
 #include <algorithm>
 #include <math.h>
 
+int tribonacci(int N){
+  if (N <= 1){
+    return 0;
+  } else if (N == 2){
+    return 1;
+  } else{
+    return tribonacci(N-1) + tribonacci(N-2) + tribonacci(N-3);
+  }
+  return 0;
+}
+
 int main() {
 
   std::vector<long> adaptors;
@@ -17,10 +28,11 @@ int main() {
   std::sort(adaptors.begin(), adaptors.end());
   adaptors.push_back(device_joltage);
 
-  long current_joltage = 0;
+  long num_adaptors = adaptors.size();
   long one_jolt_jumps = 0;
   long three_jolt_jumps = 0;
-  for (int i=1; i < adaptors.size(); i++){
+
+  for (int i=1; i < num_adaptors; i++){
     long diff = adaptors[i] - adaptors[i - 1];
     // std::cout << adaptors[i - 1] << " -> " << adaptors[i] << " : " << diff << std::endl;
     if (diff == 1) {
@@ -30,27 +42,24 @@ int main() {
     }
   }
 
-  std::cout << one_jolt_jumps << std::endl;
-  std::cout << three_jolt_jumps << std::endl;
-
-  std::cout << one_jolt_jumps*three_jolt_jumps << std::endl;
+  std::cout << "Answer Part 1: " << one_jolt_jumps*three_jolt_jumps << std::endl;
 
   long num_ways = 1;
   int ones_in_row = 0;
 
-  for (int i = 1; i < adaptors.size(); i++) {
+  for (int i = 1; i < num_adaptors; i++) {
     long diff = adaptors[i] - adaptors[i - 1];
     if (diff == 1) {
       ones_in_row++;
     } else if (diff == 3) {
       if (ones_in_row != 0){
-        int add = std::pow(2, ones_in_row-1) - (ones_in_row-1)/3;
+        int add = tribonacci(ones_in_row + 2);
         num_ways *= add;
-        std::cout << ones_in_row << " " << add << " " << (ones_in_row-1) / 3 << " "
+        std::cout << ones_in_row << " " << add << " "
                   << num_ways << std::endl;
         ones_in_row = 0;
       }
     }
   }
-  std::cout << num_ways << std::endl;
+  std::cout << "Answer Part 2: " <<num_ways << std::endl;
 }
