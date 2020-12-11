4f46121bdd72cd4a0cdee554c84fb09282e366ce
Devin Crichton <devin.crichton@gmail.com>
2020-12-11 15:03:15 +0100

    Changed from char arrays to vectors of strings
    - Performance worse than I would expect. Might investigate more.

--- 11/aoc11.cpp
+++ 11/aoc11.cpp
@@ -1,159 +1,143 @@
 #include <algorithm>
 #include <iostream>
+#include <iterator>
 #include <map>
 #include <set>
 #include <string>
 #include <unordered_map>
 #include <vector>
- 
-int main() {
 
-  std::vector<std::string> vector_of_strings;
 
-  for (std::string line; std::getline(std::cin, line);){
-    vector_of_strings.push_back(line);
-  }
-
-  int Nrow = vector_of_strings.size();
-  int Ncol = vector_of_strings[0].size();
-
-  char seat_array[Nrow][Ncol];
-
-  for (int i=0; i < Nrow; i++){
-    for (int j=0; j < Ncol; j++){
-      seat_array[i][j] = vector_of_strings[i][j];
-    }
-  }
-
-  std::vector<int> adjacency_row = {
+std::vector<int> adjacency_row = {
     -1, -1, -1,
      0,      0,
      1,  1,  1,
   };
-  std::vector<int> adjacency_col = {
+std::vector<int> adjacency_col = {
     -1, 0, 1,
     -1,    1,
     -1, 0, 1
   };
 
+std::string find_immediate_neighbours(int i, int j,
+                                      const std::vector<std::string>& seats) {
+  const int Nrow = seats.size();
+  const int Ncol = seats[0].size();
+  std::string adjacent;
+  for (int k = 0; k < 8; k++) {
+    int curr_i = i + adjacency_row[k];
+    int curr_j = j + adjacency_col[k];
+    if ((curr_i < 0) || (curr_j < 0) || (curr_i >= Nrow) || (curr_j >= Ncol)) {
+      continue;
+    } else {
+      adjacent.push_back(seats[curr_i][curr_j]);
+    }
+  }
+  return adjacent;
+}
+
+std::string find_directional_neighbours(int i, int j,
+                                        const std::vector<std::string> &seats) {
+  const int Nrow = seats.size();
+  const int Ncol = seats[0].size();
+  std::string adjacent;
+
+  for (int k = 0; k < 8; k++) {
+    int count_path = 1;
+    bool found_or_edge = false;
+    while (not found_or_edge) {
+      int curr_i = i + adjacency_row[k] * count_path;
+      int curr_j = j + adjacency_col[k] * count_path;
+      if ((curr_i < 0) || (curr_j < 0) || (curr_i >= Nrow) ||
+          (curr_j >= Ncol)) {
+        found_or_edge = true;
+      } else if (seats[curr_i][curr_j] != '.') {
+        adjacent.push_back(seats[curr_i][curr_j]);
+        found_or_edge = true;
+      }
+      count_path++;
+    }
+  }
+  return adjacent;
+}
+
+int main() {
+
+  std::vector<std::string> start_seats;
+
+  for (std::string line; std::getline(std::cin, line);){
+    start_seats.push_back(line);
+  }
+
+  const int Nrow = start_seats.size();
+  const int Ncol = start_seats[0].size();
+
   // Part 1
   bool changed = true;
-  while (changed){
-  // int count = 0;
-  // while (count < 10){
-    changed = false;
-    char new_seat_array[Nrow][Ncol];
+  std::vector<std::string> seats = start_seats;
+  std::vector<std::string> new_seats = seats;
 
+  while (changed) {
+    changed = false;
     for (int i=0; i < Nrow; i++){
       for (int j=0; j < Ncol; j++){
-        std::string adjacent;
-        for (int k=0; k < 8; k++){
-          int curr_i = i + adjacency_row[k];
-          int curr_j = j + adjacency_col[k];
-          if ((curr_i < 0) || (curr_j < 0) ||
-              (curr_i >= Nrow) || (curr_j >= Ncol)) {
-            continue;
-          } else {
-            adjacent.push_back(seat_array[curr_i][curr_j]);
-          }
-        }
+        std::string adjacent = find_immediate_neighbours(i, j, seats);
         int adjacent_occupied = std::count(adjacent.begin(), adjacent.end(), '#');
-        int adjacent_empty = std::count(adjacent.begin(), adjacent.end(), 'L');
         // Seat empty and no seats
-        if ((seat_array[i][j] == 'L') && (adjacent_occupied==0)){
+        if ((seats[i][j] == 'L') && (adjacent_occupied==0)){
           changed = true;
-          new_seat_array[i][j] = '#';
-        } else if ((seat_array[i][j] == '#') && (adjacent_occupied >= 4)) {
+          new_seats[i][j] = '#';
+        } else if ((seats[i][j] == '#') && (adjacent_occupied >= 4)) {
           // Occupied and shy
           changed = true;
-          new_seat_array[i][j] = 'L';
+          new_seats[i][j] = 'L';
         } else {
-          new_seat_array[i][j] = seat_array[i][j];
+          new_seats[i][j] = seats[i][j];
         }
-        // std::cout << adjacent << std::endl;
       }
     }
     //  Apply changes
-    for (int i = 0; i < Nrow; i++) {
-      for (int j = 0; j < Ncol; j++) {
-        seat_array[i][j] = new_seat_array[i][j];
-      }
-    }
+    seats = new_seats;
   }
 
   int count_part1 = 0;
-  for (int i = 0; i < Nrow; i++) {
-      for (int j = 0; j < Ncol; j++) {
-        if (seat_array[i][j] == '#') {
-          count_part1++;
-        }
-      }
-    }
-  std::cout << count_part1 << std::endl;
-
-  // Reset array
-  for (int i = 0; i < Nrow; i++) {
-    for (int j = 0; j < Ncol; j++) {
-      seat_array[i][j] = vector_of_strings[i][j];
-    }
+  for (auto row : seats) {
+    count_part1 += std::count(row.begin(), row.end(), '#');
   }
+  std::cout << "Part 1: " << count_part1 << std::endl;
 
   // Part 2
   changed = true;
-  while (changed){
-    changed = false;
-    char new_seat_array[Nrow][Ncol];
+  seats = start_seats;
+  new_seats = seats;
 
-    for (int i=0; i < Nrow; i++){
-      for (int j=0; j < Ncol; j++){
-        std::string adjacent;
-        for (int k=0; k < 8; k++){
-          int count_path = 1;
-          bool found_or_edge = false;
-          while (not found_or_edge){
-            int curr_i = i + adjacency_row[k]*count_path;
-            int curr_j = j + adjacency_col[k]*count_path;
-            if ((curr_i < 0) || (curr_j < 0) ||
-                (curr_i >= Nrow) || (curr_j >= Ncol)) {
-              found_or_edge = true;
-            } else if (seat_array[curr_i][curr_j] != '.'){
-              adjacent.push_back(seat_array[curr_i][curr_j]);
-              found_or_edge = true;
-            }
-            count_path++;
-          }
-        }
-        int adjacent_occupied = std::count(adjacent.begin(), adjacent.end(), '#');
-        int adjacent_empty = std::count(adjacent.begin(), adjacent.end(), 'L');
+  while (changed) {
+    changed = false;
+    for (int i = 0; i < Nrow; i++) {
+      for (int j = 0; j < Ncol; j++) {
+        std::string adjacent = find_directional_neighbours(i, j, seats);
+        int adjacent_occupied =
+            std::count(adjacent.begin(), adjacent.end(), '#');
         // Seat empty and no seats
-        if ((seat_array[i][j] == 'L') && (adjacent_occupied==0)){
+        if ((seats[i][j] == 'L') && (adjacent_occupied == 0)) {
           changed = true;
-          new_seat_array[i][j] = '#';
-        } else if ((seat_array[i][j] == '#') && (adjacent_occupied >= 5)) {
+          new_seats[i][j] = '#';
+        } else if ((seats[i][j] == '#') && (adjacent_occupied >= 5)) {
           // Occupied and shy
           changed = true;
-          new_seat_array[i][j] = 'L';
+          new_seats[i][j] = 'L';
         } else {
-          new_seat_array[i][j] = seat_array[i][j];
+          new_seats[i][j] = seats[i][j];
         }
-        // std::cout << adjacent << std::endl;
       }
     }
     //  Apply changes
-    for (int i = 0; i < Nrow; i++) {
-      for (int j = 0; j < Ncol; j++) {
-        seat_array[i][j] = new_seat_array[i][j];
-      }
-    }
+    seats = new_seats;
   }
 
   int count_part2 = 0;
-  for (int i = 0; i < Nrow; i++) {
-      for (int j = 0; j < Ncol; j++) {
-        if (seat_array[i][j] == '#') {
-          count_part2++;
-        }
-      }
-    }
-  std::cout << count_part2 << std::endl;
+  for (auto row : seats) {
+    count_part2 += std::count(row.begin(), row.end(), '#');
+  }
+  std::cout << "Part 2: " << count_part2 << std::endl;
 }
